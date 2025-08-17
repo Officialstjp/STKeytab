c44bd5574af1574d82b330d56bbfd532eb618ee1
OfficialStjp <stefanploch00@gmail.com>
2025-08-17 10:35:33 +0200

    cleanup

--- STkrbKeytab.psm1
+++ STkrbKeytab.psm1
@@ -14,17 +14,21 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 $here = $PSScriptRoot
 
-function .source([string]$rel) { . (Join-Path $here $rel) } # imports dont work this way
-
 # Private first (sorted; allow 00., 10., … prefixes)
 Get-ChildItem "$here/Private" -Filter *.ps1 | Sort-Object Name |
   ForEach-Object { . (Join-Path $here "Private/$($_.Name)") }
-                # { .source "Private/$($_.Name)" }
 
 # Public next
 $pub = Get-ChildItem "$here/Public" -Filter *.ps1 | Sort-Object Name
-$pub | ForEach-Object { . (Join-Path $here "Public/$($_.Name)") }
-                # { .source "Public/$($_.Name)" }
+$pub | ForEach-Object { 
+  try {
+    . (Join-Path $here "Public/$($_.Name)") 
+  } catch {
+    Write-Error "Failed to dot-source Public/$($_.Name): $_"
+  }
+  
+}
 
 # Export public only (filename == function name)
 Export-ModuleMember -Function $pub.BaseName
+
